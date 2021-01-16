fcb5c15e924344cd1404af15549588547d09ee06
Nerea Garcia <nereitagp12@gmail.com>
2021-01-16 19:36:38 +0100

    Added methods to Battle

--- src/com/ironhack/classes/Battle.java
+++ src/com/ironhack/classes/Battle.java
@@ -1,9 +1,16 @@
 package com.ironhack.classes;
 
+import com.ironhack.interfaces.Atacker;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Battle {
+
+    public static int TIE = 0;
+    public static int FIRST_CHARACTER_WIN = 1;
+    public static int SECOND_CHARACTER_WIN = 2;
 
     private Party firstParty;
     private Party secondParty;
@@ -12,6 +19,48 @@ public class Battle {
     public Battle(Party firstParty, Party secondParty) {
         this.firstParty = firstParty;
         this.secondParty = secondParty;
+    }
+
+    public Party fight(){
+        Scanner scanner = new Scanner(System.in);
+        while(firstParty.hasRemainingCharacters() && secondParty.hasRemainingCharacters()){
+            System.out.println("Select a character from first party to fight by position");
+            firstParty.printCharactersInfo();
+            int selectedFighterFirstParty = scanner.nextInt();
+            System.out.println("Select a character from second party to fight by position");
+            secondParty.printCharactersInfo();
+            int selectedFighterSecondParty = scanner.nextInt();
+            Character firstFighter = firstParty.getCharacters().get(selectedFighterFirstParty);
+            Character secondFighter = secondParty.getCharacters().get(selectedFighterSecondParty);
+            int result = fight(firstFighter,secondFighter);
+            if(result == 1){
+                graveyard.add(secondFighter);
+                secondParty.removeCharacter(secondFighter);
+            } else if(result == 0){
+                graveyard.add(firstFighter);
+                firstParty.removeCharacter(firstFighter);
+            }
+        }
+        if(firstParty.hasRemainingCharacters()){
+            return firstParty;
+        } else{
+            return secondParty;
+        }
+    }
+
+    public int fight(Character firstFighter, Character secondFighter){
+        while(firstFighter.isAlive && secondFighter.isAlive){
+            firstFighter.getHurt(((Atacker) secondFighter).attack());
+            secondFighter.getHurt(((Atacker) firstFighter).attack());
+            firstFighter.checkAlive();
+            secondFighter.checkAlive();
+        }
+        if(!firstFighter.isAlive && !secondFighter.isAlive) return TIE;
+        if(firstFighter.isAlive) {
+            return FIRST_CHARACTER_WIN;
+        } else{
+            return SECOND_CHARACTER_WIN;
+        }
     }
 
     public Party getFirstParty() {
